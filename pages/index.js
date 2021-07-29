b95558d2cb3f10174c10533f278f1e7c341995a3
Rhoman James <rhomanmjames@gmail.com>
2021-07-29 03:38:54 -0400

    Body complete

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Body from '../components/Body'
+
+
 
 export default function Home() {
   return (
@@ -9,12 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/*Header*/}
-      <h1>testing</h1>
       <Header />
       {/*Body*/}
-      <body>
-        <h2>body</h2>
-      </body>
+      <Body />
     </div>
   )
 }
